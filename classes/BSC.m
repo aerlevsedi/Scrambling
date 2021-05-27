58c2ad6f34ebcaa18316ac01325fec2ea81ed6d6
tozaist <toz4ist@gmail.com>
2021-05-27 04:03:47 +0200

    Implemented BSC - send, receive

---
+++ classes/BSC.m
@@ -0,0 +1,40 @@
+classdef BSC < handle
+
+    properties
+        signal
+        probability % crossover probability
+    end
+
+    methods
+        function obj = BSC()
+            obj.signal = [];
+            obj.probability = 0;
+        end
+
+        function setProb(obj, prob)
+            obj.probability = prob;
+        end
+
+        function send(obj, signal)
+            obj.signal = signal.copy;
+        end
+
+        function o = receive(obj)
+            if isempty(obj.signal)
+                o = Signal(0);
+            else
+                o = obj.signal;
+
+                for i = 1 : o.getSize()
+                    if rand < obj.probability
+                        o.negBit(i);
+                        msg = ['Bit ' num2str(i) ' was flipped'];
+                        disp(msg);
+                    end
+                end
+
+                obj.signal = [];
+            end
+        end
+    end
+end
